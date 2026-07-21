3481c8b42a7000fdc4181162660ad8a66e0dec4b
Jeffrey Perlin <jeffrey_perlin@yahoo.com>
2026-07-21 17:34:54 -0400

    Added annotations and tags to pw.basic.cap.make.aptmnt.spec.ts, login.spec.ts. Updated package.json file to contain new scripts that will execute based on tags using --grep

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,6 +55,7 @@ export default defineConfig({
         ignoreHTTPSErrors: true,
         navigationTimeout: 30_000, // // Numeric separators improve readability: 30_000 equals 30,000 milliseconds.
         screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
     },
 
     /* Configure projects for major browsers */
--- tests/functional/login.spec.ts
+++ tests/functional/login.spec.ts
@@ -1,40 +1,42 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test.describe("Login functionality", () => {
-  test.beforeEach("Go to login page", async ({ page }) => {
-    await page.goto("https://katalon-demo-cura.herokuapp.com/");
-    await expect(page).toHaveTitle("CURA Healthcare Service");
-    await expect(page.locator("//h1")).toHaveText("CURA Healthcare Service");
+test.describe(
+    'Login functionality',
+    { annotation: { type: 'User Story', description: 'This is the test description.' }, tag: '@regression' },
+    () => {
+        test.beforeEach('Go to login page', async ({ page }) => {
+            await page.goto('https://katalon-demo-cura.herokuapp.com/');
+            await expect(page).toHaveTitle('CURA Healthcare Service');
+            await expect(page.locator('//h1')).toHaveText('CURA Healthcare Service');
 
-    // Click on 'Make Appointment' link
-    await page.getByRole("link", { name: "Make Appointment" }).click();
-    // assert that 'Please login to make' will be visible
-    await expect(page.getByText("Please login to make")).toBeVisible();
-  });
+            // Click on 'Make Appointment' link
+            await page.getByRole('link', { name: 'Make Appointment' }).click();
+            // assert that 'Please login to make' will be visible
+            await expect(page.getByText('Please login to make')).toBeVisible();
+        });
 
-  test("Should login successfully", async ({ page }) => {
-    // Launch URL and assert title and header
+        test('Should login successfully', { tag: '@smoke' }, async ({ page }) => {
+            // Launch URL and assert title and header
 
-    // Successful Login
-    await page.getByLabel("Username").fill("John Doe");
-    await page.getByLabel("Password").fill("ThisIsNotAPassword");
-    await page.getByRole("button", { name: "Login" }).click();
+            // Successful Login
+            await page.getByLabel('Username').fill('John Doe');
+            await page.getByLabel('Password').fill('ThisIsNotAPassword');
+            await page.getByRole('button', { name: 'Login' }).click();
 
-    // Verify 'Make Appointment' text after attempting to log in
-    await expect(page.locator("h2")).toContainText("Make Appointment");
-  });
+            // Verify 'Make Appointment' text after attempting to log in
+            await expect(page.locator('h2')).toContainText('Make Appointment');
+        });
 
-  // Negative test (invalid credentials) - assert login failed message is displayed
-  test("Should prevent login with incorrect creds", async ({ page }) => {
-    // Failed Login
-    await page.getByLabel("Username").fill("John Smith");
-    await page.getByLabel("Password").fill("ThisIsNotAPassword");
-    await page.getByRole("button", { name: "Login" }).click();
+        // Negative test (invalid credentials) - assert login failed message is displayed
+        test('Should prevent login with incorrect creds', async ({ page }) => {
+            // Failed Login
+            await page.getByLabel('Username').fill('John Smith');
+            await page.getByLabel('Password').fill('ThisIsNotAPassword');
+            await page.getByRole('button', { name: 'Login' }).click();
 
-    // Verify unsuccessful login
+            // Verify unsuccessful login
 
-    await expect(page.locator("#login")).toContainText(
-      "Login failed! Please ensure the username and password are valid.",
-    );
-  });
-});
+            await expect(page.locator('#login')).toContainText('Login failed! Please ensure the username and password are valid.');
+        });
+    }
+);
